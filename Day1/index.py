15b5aeb1ba4b7e4a4f20973c3090e86a9f4a967b
Bryan <bryan.r@avense-tech.com>
2025-11-24 13:18:06 +0800

    add day 1

---
+++ Day1/index.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("Enter the name of the city you grew up in:\n")
+pet = input("Enter the name of your pet:\n")
+print("Your band name is:", city + " " + pet)
